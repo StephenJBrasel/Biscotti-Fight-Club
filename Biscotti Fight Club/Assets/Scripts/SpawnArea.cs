92567e4d2b8c392940f74f08a90b75ea7da43bdb
Stephen Brasel <wearyroad@gmail.com>
2020-01-09 11:52:44 -0500

    RushThePlayer Moves the attached object towards a randomly selected (at initialization) player.
    Also pushing initial version of a spawning area. May need work.

---
+++ Biscotti Fight Club/Assets/Scripts/SpawnArea.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[System.Serializable]
+public class SpawnType
+{
+    [Tooltip("The prefab that will be instantiated.")]
+    public GameObject prefab;
+
+    [Tooltip("The maximum amount to spawn at one time.")] 
+    public int max;
+
+    [Tooltip("The minimum amount to spawn at one time.")]
+    public int min;
+
+}
+
+public class SpawnArea : MonoBehaviour
+{
+    [SerializeField] private SpawnType[] spawnTypes;
+    [SerializeField] private float yPos = 0f;
+    [SerializeField] private float spawnRate = 3f;
+
+    private Rect AreaToSpawnIn;
+    private float xMin;
+    private float xMax;
+    private float zMin;
+    private float zMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = transform.position.x - (transform.localScale.x * 0.5f);
+        xMax = xMin + transform.localScale.x;
+
+        zMin = transform.position.z - (transform.localScale.z * 0.5f);
+        zMax = zMin + transform.localScale.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        foreach( SpawnType spawn in spawnTypes)
+        {
+            int amount = Random.Range(spawn.min, spawn.max);
+            for (int i = 0; i < amount; i++)
+            {
+                float xPos = Random.Range(xMin, xMax);
+                float zPos = Random.Range(zMin, zMax);
+                Vector3 startPos = new Vector3(xPos, yPos, zPos);
+                Instantiate(spawn.prefab, startPos, Quaternion.identity);
+            }
+        }
+    }
+}
