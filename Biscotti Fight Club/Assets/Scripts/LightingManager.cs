aa581910b43e3b6f1633afc5be35978ed6141bed
DaiDai19 <daidai19@users.noreply.github.com>
2020-01-09 14:49:34 -0500

    Additions
    I made a scriptable object for the lighting, imported a coffee mug into the scene, made a day and night cycle

---
+++ Biscotti Fight Club/Assets/Scripts/LightingManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset preset;
+    [SerializeField, Range(0, 100)] private float timeOfDay;
+
+
+
+    private void Update()
+    {
+        if (preset == null)
+            return;
+
+        if(Application.isPlaying)
+        {
+            timeOfDay += Time.deltaTime;
+            timeOfDay %= 100;
+            UpdateLighting(timeOfDay / 100);
+        }
+        else
+        {
+            UpdateLighting(timeOfDay / 100);
+        }
+    }
+
+
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = preset.fontColor.Evaluate(timePercent);
+        
+        if(directionalLight != null)
+        {
+            directionalLight.color = preset.directionColor.Evaluate(timePercent);
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360) -90, -170, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if(directionalLight != null)
+            return;
+
+        if(RenderSettings.sun != null)
+        {
+            directionalLight = RenderSettings.sun;
+        }
+
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach(Light light in lights)
+            {
+                if(light.type == LightType.Directional)
+                {
+                    directionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Biscotti Fight Club/Assets/Scripts/LightingPreset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenuAttribute (fileName = "Lighting Preset", menuName = "Scriptables/Lighting")]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient ambientColor;
+    public Gradient directionColor;
+    public Gradient fontColor;
+}
