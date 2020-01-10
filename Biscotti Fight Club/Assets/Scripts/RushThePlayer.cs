57c488bc92d29d90ea8985fa62c468a7328d408f
Stephen Brasel <wearyroad@gmail.com>
2020-01-10 09:55:24 -0500

    Bear animations work.

--- Biscotti Fight Club/Assets/Scripts/RushThePlayer.cs
+++ Biscotti Fight Club/Assets/Scripts/RushThePlayer.cs
@@ -6,40 +6,46 @@ using Random = UnityEngine.Random;
 [RequireComponent(typeof(CharacterController))]
 public class RushThePlayer : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float attackThreshold = 2f;
-    [SerializeField] private Animator animator;
+	[SerializeField] private float speed;
+	[SerializeField] private float attackThreshold = 2f;
+	[SerializeField] private Animator animator;
 
-    private Transform PlayerPos;
-    private CharacterController m_CharacterController;
+	private Transform PlayerPos;
+	private CharacterController m_CharacterController;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_CharacterController = GetComponent<CharacterController>();
-        if (animator == null) animator = GetComponent<Animator>();
-        if (animator) animator.SetBool("Chase", true);
+	// Start is called before the first frame update
+	void Start()
+	{
+		m_CharacterController = GetComponent<CharacterController>();
+		if (animator == null) animator = GetComponent<Animator>();
+		if (animator) animator.SetBool("Chase", true);
 
-        //Choose random player to bullrush.
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        PlayerPos = players[Random.Range(0, players.Length)].transform;
-    }
+		//Choose random player to bullrush.
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		PlayerPos = players[Random.Range(0, players.Length)].transform;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if not close to player, move towards the player pos.
-        transform.LookAt(PlayerPos);
-        if(Vector3.Distance(PlayerPos.position, transform.position) > attackThreshold)
-        {
-            if (animator) animator.SetBool("Chase", true);
-            m_CharacterController.Move(transform.forward * speed * Time.deltaTime);
-        }
-        else
-        {
-            //attack
-            if (animator) animator.SetBool("Chase", false);
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		//if not close to player, move towards the player pos.
+		transform.LookAt(PlayerPos);
+		if (Vector3.Distance(PlayerPos.position, transform.position) > attackThreshold)
+		{
+			Vector3 moveDir = transform.forward * speed * Time.deltaTime;
+			if (!m_CharacterController.isGrounded)
+			{
+				moveDir += Physics.gravity;
+			}
+
+			if (animator) animator.SetBool("Chase", true);
+			m_CharacterController.Move(moveDir);
+		}
+		else
+		{
+			//attack
+			if (animator) animator.SetBool("Chase", false);
+		}
+	}
 }
